5be473f036e31a7bb78ac1350cb74a175f3e12c7
Jessicarrr <JWP01993@gmail.com>
2023-12-27 19:11:09 +1000

    Update player.tscn, update status bars
    - Updated player.tscn to match the rest of the codebase, because player.tscn was not updated properly
    - Added status bars (stamina, health) to their own scene

--- ui/HealthBar.gd
+++ ui/HealthBar.gd
@@ -12,6 +12,7 @@ func _ready():
 	health_node = get_node(health_node_path)
 	self.value = health_node.current_hitpoints
 	self.max_value = health_node.max_hitpoints
+	health_node.health_changed.connect(_on_health_health_changed)
 
 func _on_health_health_changed(current_hp, max_hp):
 	self.value = current_hp
