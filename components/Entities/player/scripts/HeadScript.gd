6a83435ea6e1a45464adbce90fdb9782a0786004
Jessicarrr <JWP01993@gmail.com>
2023-12-29 01:16:09 +1000

    add smooth transition to camera crouching

--- components/Entities/player/scripts/HeadScript.gd
+++ components/Entities/player/scripts/HeadScript.gd
@@ -1,16 +1,28 @@
 extends Node3D
 
 @onready var player = get_parent()
-@onready var default_position = self.transform.origin
+@onready var default_position = self.position
 var crouched_position = Vector3(0, 0.23, 0)
 var crouched = false
+var tween
+
+func create_new_tween(parallel, tween_type):
+	if tween != null:
+		if tween.is_running():
+			tween.stop()
+			
+	tween = get_tree().create_tween()
+	tween.set_parallel(parallel)
+	tween.set_ease(tween_type)
 
 func _on_crouch_started():
-	self.transform.origin = crouched_position
+	create_new_tween(true, Tween.EASE_IN_OUT)
+	tween.tween_property(self, "position", crouched_position, player.crouch_transition_time)
 	crouched = true
 	
 func _on_crouch_ended():
-	self.transform.origin = default_position
+	create_new_tween(true, Tween.EASE_IN_OUT)
+	tween.tween_property(self, "position", default_position, player.crouch_transition_time)
 	crouched = false
 
 # Called when the node enters the scene tree for the first time.
