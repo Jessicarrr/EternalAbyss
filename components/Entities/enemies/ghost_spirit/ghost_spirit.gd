ebfd77ec2eeb41aaa0164ce8f2a935b26e144a30
Jessicarrr <JWP01993@gmail.com>
2024-03-06 17:11:01 +1000

    add ghost flashing when ghost in certain states

--- components/Entities/enemies/ghost_spirit/ghost_spirit.gd
+++ components/Entities/enemies/ghost_spirit/ghost_spirit.gd
@@ -3,6 +3,12 @@ class_name GhostSpiritNpc
 
 @export var visibility_range = 8.0
 
+@export_category("Ghost Stuff")
+@export var flashing_visibility_time = 0.4
+@export var flashing_invisibility_time = 0.7
+@export var flashing_randomness_range = 0.1
+@export var always_tangible = false
+
 signal on_hit
 
 #@onready var health = $Skills/Health
