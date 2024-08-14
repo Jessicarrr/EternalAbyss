00aab24d492248613c947584eaaab974de3ef09f
Jessicarrr <JWP01993@gmail.com>
2024-08-14 17:50:44 +1000

    fix bug where delete parent chance not done correctly

--- world/shared/scripts/random_chance_delete_parent.gd
+++ world/shared/scripts/random_chance_delete_parent.gd
@@ -5,7 +5,7 @@ class_name RandomChanceDeleteParent
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var double_chance : float = float(chance_of_deletion) / 10
+	var double_chance : float = float(chance_of_deletion) / 100
 
 	if Helpers.should_chance_succeed(double_chance):
 		get_parent().queue_free()
